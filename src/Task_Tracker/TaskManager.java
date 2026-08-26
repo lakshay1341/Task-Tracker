1cf1aa887df4ecb5d9c6dfc7af2dbcd44e5fdffa
Lakshay Chaudhary <63228418+chaudhary-lakshay@users.noreply.github.com>
2026-08-26 10:12:40 +0530

    chore: normalize line endings for PR 9 resolution

--- src/Task_Tracker/TaskManager.java
+++ src/Task_Tracker/TaskManager.java
@@ -46,9 +46,9 @@ public class TaskManager {
 
     public void addTask(String description) {
         int id = tasks.stream()
-                .mapToInt(Task::getId)
-                .max()
-                .orElse(0) + 1;
+        .mapToInt(Task::getId)
+        .max()
+        .orElse(0) + 1;
         Task task = new Task(id, description);
         tasks.add(task);
         saveTasks();
